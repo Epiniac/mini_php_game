fbc8ad963520fbd1d165455e721807c1fb92f822
cognitech <j_chen39@hetic.eu>
2024-05-14 10:16:14 +0200

    class and heritage added

--- jeu.php
+++ jeu.php
@@ -1,15 +1,14 @@
 <?php
 
-require_once 'personnage.php';
-require_once 'combat.php';
+require_once './Combat.php';
+require_once './Archer.php';
+require_once './Personnage.php';
 
 $combat = new Combat;
 
-$j1 = new Personnage('joueur 1');
-$j2 = new Personnage('joueur 2');
+$j1 = new Archer('Joueur 1');
+$j2 = new Personnage('Joueur 2');
 
-$combat -> prendrePart($j1,$j2);
+$combat->prendrePart($j1, $j2);
 
-$combat -> seDerouler();
-
-
+$combat->seDerouler();
--- personnage.php
+++ personnage.php
@@ -1,54 +1,75 @@
 <?php
-include './combat.php';
 
- class Personnage {
+require_once './Combat.php';
+require_once './Narrateur.php';
+
+class Personnage {
     public string $nom;
-    public int $pv =100;
-
-    public function __construct(string $nom) {
-        $this->$nom = $nom;
-    }
+    public int $pdv = 100;
 
     const CHOIX = [
-    	"attaquer",
-	"fuir"
+        'attaquer' => 'Attaquer un autre personnage',
+        'fuir' => 'fuir'
     ];
 
-    function attaquer(Personnage $qqdautre){
-	$qqdautre -> pdv -= rand(0,50); 
+    function __construct(string $nom) {
+        $this->nom = $nom;
     }
 
-    function fuir(Combat $c): bool{
-	if(rand(0,1)){
-		$c -> abandonner($this);
-		return True;
-	}	
-	else{
-		return False;
-	}
+    function attaquer(Personnage $qqunDAutre) {
+        Narrateur::parler('J\'attaque ' . $qqunDAutre->nom, $this);
+        $qqunDAutre->pdv -= ($i = rand(0, 50));
+        Narrateur::parler($qqunDAutre->nom . ' perd ' . $i . ' PV');
     }
 
-    function effectuerTour(Combat $c) {
-	while(
-		!in_array{
-			$choix = readLine('Que souhaitez-vous faire ? '
-				. PHP_EOL . '1. Attaquer'
-				. PHP_EOL . '2. Fuir');
-			[1,2]
-		}    	
-	){
-		switch($choix) {
-			case 1:
-				$choix = -1;
-				while(!isset($combat->joueurs[$choix])) {
-					echo 'Qui souhaitez-vous attaquer ? ' . PHP_EOL;
-					foreach ($combat->$joueurs as $key->$joueur){
-						echo $key . ':' . $joueur->nom . PHP_EOL;
-					}
-					$choix = readLine();
-				}
-				$cible = $combat->$joueur 
-		}
-	}
+    function fuir(Combat $combat): bool {
+        Narrateur::parler('Je tente de prendre la fuite', $this);
 
+        if (rand(0, 1)) {
+            $combat->abandonner($this);
+            Narrateur::parler('Je prends la fuite', $this);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function effectuerTour(Combat $combat) {
+        while (
+            !in_array(
+                $choix = readline('Que voulez-vous faire ? '
+                    . PHP_EOL . '1: Attaquer'
+                    . PHP_EOL . '2: Prendre la fuite'
+                    . PHP_EOL),
+                [1, 2]
+            )
+        );
+
+        switch ($choix) {
+            case 1:
+                $choix = -1;
+                while (!isset($combat->joueurs[$choix])) {
+                    echo 'Qui voulez-vous attaquer ?' . PHP_EOL;
+                    foreach ($combat->joueurs as $key => $joueur) {
+                        echo $key . ': ' . $joueur->nom . PHP_EOL;
+                    }
+
+                    $choix = readline();
+                }
+
+                $cible = $combat->joueurs[$choix];
+
+                $this->attaquer($cible);
+
+                if ($cible->pdv <= 0) {
+                    Narrateur::parler($cible->nom . ' est morte au champ d\'honneur.');
+                    $combat->abandonner($cible);
+                }
+                break;
+
+            case 2:
+                $this->fuir($combat);
+                break;
+        }
+    }
 }
